08c8089b942a348d31d70ffd682bb506d387b60d
str-gustavo <gustavo.kraus@transjoi.com.br>
2025-07-21 14:47:17 -0300

    Troca dos arquivos na alteração de pacote

--- app/src/androidTest/java/br/com/transjoi/quickops/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/transjoi/quickops/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.quickdevjav;
+package br.com.transjoi.quickops;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.quickdevjav", appContext.getPackageName());
+        assertEquals("br.com.transjoi.quickops", appContext.getPackageName());
     }
 }
