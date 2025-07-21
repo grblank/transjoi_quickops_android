750098a0c5eb73c3943b2ceab1ee84aec4750a87
str-gustavo <gustavo.kraus@transjoi.com.br>
2025-07-21 14:52:13 -0300

    Novo MainActivity, tirando as funções de instalação de Apk, tirando a interceptação das requisições para adicionar header

---
+++ app/src/main/java/br/com/transjoi/quickops/MyWebViewClient.java
@@ -0,0 +1,29 @@
+package br.com.transjoi.quickops;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+
+public class MyWebViewClient extends WebViewClient {
+
+    private Activity activity = null;
+
+    public void WebViewClientImpl(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest url) {
+        if(url.getUrl().toString().indexOf("http://192.168.0.6:8000") > -1 ) return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url.getUrl().toString()));
+        activity.startActivity(intent);
+        return true;
+    }
+
+}
